deb8136f45a0421426fef4d3a5967ffbd6fde96a
Ansh8096 <anshv8096@gmail.com>
2026-09-06 15:43:44 +0530

    Refine application bootstrap and TypeScript configuration

--- src/main.tsx
+++ src/main.tsx
@@ -1,13 +1,47 @@
-import { createRoot } from 'react-dom/client'
-import { BrowserRouter } from 'react-router-dom'
-import './index.css'
-import App from './App.tsx'
-import { AuthProvider } from './contexts/AuthContext.tsx'
+import { createRoot } from "react-dom/client";
+import { BrowserRouter } from "react-router-dom";
 
-createRoot(document.getElementById('root')!).render(
-  <BrowserRouter>
-    <AuthProvider>
-      <App/>
-    </AuthProvider>
-  </BrowserRouter>
-)
+import "./index.css";
+
+import App from "./App.tsx";
+
+import { AuthProvider } from "./contexts/AuthContext.tsx";
+import {
+    ThemeProvider,
+} from "./contexts/ThemeContext.tsx";
+
+import { queryClient } from "@/lib/react-query";
+
+import {
+    QueryClientProvider,
+} from "@tanstack/react-query";
+
+import {
+    ReactQueryDevtools,
+} from "@tanstack/react-query-devtools";
+import { SidebarProvider } from "./contexts/SidebarContext.tsx";
+
+createRoot(
+    document.getElementById("root")!,
+).render(
+    <QueryClientProvider
+        client={queryClient}
+    >
+        <ThemeProvider>
+            <BrowserRouter>
+                <SidebarProvider>
+                    <AuthProvider>
+                        <App />
+                    </AuthProvider>
+                </SidebarProvider>
+
+                {/* React Query Devtools */}
+                {import.meta.env.DEV && (
+                    <ReactQueryDevtools
+                        initialIsOpen={false}
+                    />
+                )}
+            </BrowserRouter>
+        </ThemeProvider>
+    </QueryClientProvider>,
+);
